6248aba7cbb90d9a945beba5aaf8067258524bfc
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-09 09:59:08 -0600

    221209_1000 p01 pass

--- solutions.js
+++ solutions.js
@@ -19,13 +19,29 @@
  * isFactor
  */
 
+//problem01
+// Define a function named isGreaterThan that takes in two values and returns a true if the first argument is greater than the second argument otherwise it should return false. If either argument is not a number, return false;
+
+function isGreaterThan(a,b){
+    if (isNaN(a) || isNaN(b) === true){
+        return false
+    }
+    if (parseFloat(a) > parseFloat(b) === true){
+        return true
+    }
+     return false
+}
+
+
+
 function add(a,b){
     if (isNaN(parseFloat(a) + parseFloat(b))){
         return false
     }
     return parseFloat(a) + parseFloat(b)
 }
-console.log(add(3.3,4));
+
+    console.log(add(3.3,4));
 
 function calcCube(a){
     if (typeof a === `number`){
